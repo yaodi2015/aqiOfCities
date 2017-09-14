390fd31b2749305311d0091a0ee50e0c6633e2e8
yaodi0901 <yaodi@medlinker.com>
2017-09-14 14:44:11 +0800

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// const pxtorem = require("postcss-pxtorem");//for ali 高清配置
+const autoprefixer = require("autoprefixer");
+module.exports = {
+	plugins: [
+		autoprefixer,
+		// pxtorem({
+		// 	rootValue: 100
+		// })
+  ]
+};
